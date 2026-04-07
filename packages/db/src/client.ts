5f370b2150deee26718f6dafc5a4fc21cfe9de7b
Shubham Amrawat <kumarjordy12@gmail.com>
2026-04-08 00:16:06 +0530

    feat: fix auth/me coverUrl + R2 image domain config

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,9 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import dns from "node:dns";
 import * as schema from "./schema/users";
 import * as wallpaperSchema from "./schema/wallpapers";
 import * as collectionSchema from "./schema/collections";
 import * as interactionSchema from "./schema/interactions";
+import * as authSchema from "./schema/auth";
 
 function createDb() {
   const connectionString = process.env.DATABASE_URL;
@@ -12,7 +14,9 @@ function createDb() {
     throw new Error("DATABASE_URL environment variable is not set");
   }
 
-  console.log("Creating DB connection to:", connectionString.slice(0, 40));
+  // Render/Vercel containers often have no IPv6 route; this prevents DNS from
+  // selecting AAAA records first (which caused ENETUNREACH in production).
+  dns.setDefaultResultOrder("ipv4first");
 
   const client = postgres(connectionString, {
     ssl: "require",
@@ -27,6 +31,7 @@ function createDb() {
       ...wallpaperSchema,
       ...collectionSchema,
       ...interactionSchema,
+      ...authSchema,
     },
   });
 }
